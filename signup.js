63be2e3b26472b50caa3e79906c1464943b7aeb8
unknown <zaroosadaf@gmail.com>
2026-07-23 11:18:58 +0530

    added signup files

---
+++ signup.js
@@ -0,0 +1,21 @@
+function validateForm() {
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let confirm = document.getElementById("confirm").value;
+    let message = document.getElementById("message");
+
+    if(name=="" || email=="" || password=="" || confirm==""){
+        message.innerHTML="Please fill all fields.";
+        return false;
+    }
+
+    if(password !== confirm){
+        message.innerHTML="Passwords do not match.";
+        return false;
+    }
+
+    alert("Sign Up Successful!");
+    return true;
+}

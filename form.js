2fff30fc470e68632283886ae9d4de0f855c63be
aqsa <iaqsa6@gmail.com>
2026-07-24 00:34:50 +0530

    added some changes

--- form.js
+++ form.js
@@ -1,31 +1,26 @@
-<<<<<<< HEAD
-let correctUsername="aqsakhan";
-let correctPassword="password1";
+let correctUsername = "admin";
+let correctPassword = "12345";
 
+let loginForm = document.getElementById("loginForm");
 
+loginForm.addEventListener("submit", function(event) {
 
-function login(){
-let enteredUsername=document.getElementById("username").value; 
-let enteredPassword=document.getElementById("password").value;
- if(enteredUsername === correctUsername,enteredPassword===correctPassword){
-       console.log("our credentials are=",enteredPassword ,"and", enteredUsername);
-       alert("Login Successfull");
-       window.location.href="home.html"
- }
+    event.preventDefault();
 
- 
- else{
-    alert("wrong credentials")
- }
-}
- function clearData(){
- document.getElementById("username").value ="";
- document.getElementById("password").value ="";
-       alert("cleared successfull");
-=======
-function showMessage(){
-alert("login successfull!");
-window.location.href="./registration.html"
->>>>>>> 8c47af31a75970b87b4bef675bae8f4036b544c9
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let message = document.getElementById("message");
 
-}
+    if (username === correctUsername && password === correctPassword) {
+
+        message.textContent = "Login Successful!";
+        message.style.color = "green";
+
+    } else {
+
+        message.textContent = "Wrong Credentials!";
+        message.style.color = "red";
+
+    }
+
+});

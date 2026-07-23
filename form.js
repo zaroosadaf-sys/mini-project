735429ab7462f3c657f751c06b3d73746911c405
aqsa <iaqsa6@gmail.com>
2026-07-24 00:01:52 +0530

    updated files

--- form.js
+++ form.js
@@ -1,5 +1,25 @@
-function showMessage(){
-alert("login successfull!");
-window.location.href="index.html"
+let correctUsername="aqsakhan";
+let correctPassword="password1";
+
+
+
+function login(){
+let enteredUsername=document.getElementById("username").value; 
+let enteredPassword=document.getElementById("password").value;
+ if(enteredUsername === correctUsername,enteredPassword===correctPassword){
+       console.log("our credentials are=",enteredPassword ,"and", enteredUsername);
+       alert("Login Successfull");
+       window.location.href="home.html"
+ }
+
+ 
+ else{
+    alert("wrong credentials")
+ }
+}
+ function clearData(){
+ document.getElementById("username").value ="";
+ document.getElementById("password").value ="";
+       alert("cleared successfull");
 
 }

f550bfb3ecf0aa7d53c5a26c6c765f7f1987178f
unknown <zaroosadaf@gmail.com>
2026-07-28 11:26:31 +0530

    added bootstrap links in sign up.html in head

--- signup.js
+++ signup.js
@@ -1,40 +1,37 @@
-function validateForm() {
+function validateForm(){
 
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var phone = document.getElementById("phone").value;
-    var password = document.getElementById("password").value;
-    var confirm = document.getElementById("confirm").value;
-    var terms = document.getElementById("terms").checked;
+    var name=document.getElementById("name").value;
+    var email=document.getElementById("email").value;
+    var phone=document.getElementById("phone").value;
+    var password=document.getElementById("password").value;
+    var confirm=document.getElementById("confirm").value;
+    var terms=document.getElementById("terms").checked;
 
-    if(name == "" || email == "" || phone == "" || password == "" || confirm == ""){
-        alert("Please fill all the fields.");
+    (name=="" || email=="" || phone=="" || password=="" || confirm==""){
+        alert("Please fill all fields");
     }
 
-    if(password != confirm){
-        alert("Passwords do not match.");
+    (password!=confirm){
+        alert("Passwords do not match")
     }
 
-    if(terms == false){
-        alert("Please accept the Terms & Conditions.");
+   if (terms==false){
+        alert("Please accept Terms & Conditions");
     }
 
     alert("Sign Up Successful!");
-    return true;
 }
 
-function showPassword() {
+function showPassword(){
 
-    var pass = document.getElementById("password");
-    var con = document.getElementById("confirm");
+    var pass=document.getElementById("password");
 
-    if(pass.type == "password"){
-        pass.type = "text";
-        con.type = "text";
+    if(pass.type=="password"){
+        pass.type="text";
     }
     else{
-        pass.type = "password";
-        con.type = "password";
+        pass.type="password";
     }
     window.location.href="./form.html"
 }
+
